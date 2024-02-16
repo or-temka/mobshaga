ef5e184bf2ee4f433a1a9282b23b1c8340a94430
artem schegortzov <tema.chegortzov@mail.ru>
2024-02-17 00:51:07 +0300

    done frontend for create account page

--- pages/profile_pages/sign_up/main.js
+++ pages/profile_pages/sign_up/main.js
@@ -20,3 +20,17 @@ const profilePhotoUploader = new SinglePhotoUploader(
   }
 );
 
+//set disabled hotel-room if person not from hotel
+const inputForDisable = document.querySelector(".sign-up__input--hotel-room");
+inputForDisable.setAttribute("disabled", "true");
+inputForDisable.setAttribute("placeholder", "Недоступно");
+document.getElementById("hotel-number-select").addEventListener("change", (event) => {
+  if (event.target.value === "null"){
+    inputForDisable.setAttribute("disabled", "true");
+    inputForDisable.setAttribute("placeholder", "Недоступно");
+    inputForDisable.value = "";
+  } else {
+    inputForDisable.removeAttribute("disabled");
+    inputForDisable.setAttribute("placeholder", "Например: 4301");
+  }
+});
