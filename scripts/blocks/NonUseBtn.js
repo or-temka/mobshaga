041f7f38efbba03eef0b4f43e5c97eec4ad5d16d
artem schegortzov <tema.chegortzov@mail.ru>
2024-02-20 18:13:31 +0300

    done notification msg

---
+++ scripts/blocks/NonUseBtn.js
@@ -0,0 +1,41 @@
+export class NonUseBtn{
+  nowNotificationActive = false;
+  //notificationBlockSelector - block of notification for set display none and block
+  //notificationTextSelector - for set text notification in block notification
+  //btnsSelector - [array] of object {btnsSelector: string: textNotificationNonUse: string}
+  //notificationDuration - duration of notification im ms [default = 4000]
+  constructor(notificationBlockSelector, notificationTextSelector, btnsSelector, warningSignSelector, notificationDuration = 4000){
+    this.notificationBlock = document.querySelector(notificationBlockSelector);
+    this.notificationText = document.querySelector(notificationTextSelector);
+    this.warningSign = document.querySelector(warningSignSelector);
+    this.notificationDuration = notificationDuration;
+    
+    this.warningSign.style.display = "block";
+    
+    for (let btn of btnsSelector){
+      const btnSelector = Object.keys(btn)[0];
+      document.querySelectorAll(btnSelector).forEach((btnElem) => {
+        btnElem.addEventListener("click", this._notUseOpenNotification.bind(this, btn[btnSelector]));
+      });
+    }
+
+  }
+  _notUseOpenNotification(textNotification){
+    if (this.nowNotificationActive){ return; }
+    this.nowNotificationActive = true;
+    this.notificationText.textContent = textNotification;
+    this.notificationBlock.style.display = "block";
+    setTimeout(() => {
+      this.notificationBlock.style.transform = "translateX(120%)";
+      this.notificationBlock.style.animation = "pop-up-fly-reverse 0.2s ease-in-out";
+      
+      setTimeout(() => {
+        this.notificationBlock.style.display = "none";
+        this.notificationBlock.style.transform = "translateX(0%)";
+        this.notificationBlock.style.animation = "pop-up-fly 0.2s ease-in-out";
+        this.nowNotificationActive = false;
+      }, 200);
+    }, textNotification.length * 80);
+  }
+  
+}
--- scripts/main.js
+++ scripts/main.js
@@ -2,28 +2,96 @@ import { start } from '/scripts/general.js'
 start();
 
 import { SortBtns } from '/scripts/blocks/product-sort.js';
+import { NonUseBtn } from './blocks/NonUseBtn.js';
 
+//non use btn
+const nonUseBtn = new NonUseBtn(
+  ".pop-up-msg",
+  ".pop-up-msg__msg",
+  [
+    {".primary-btn--category": "Извните, категории будут доступны позже. Мы уже работаем над этим."},
+    {"#shops-menu-link": "Извините, магазины будут доступны позже. Мы уже работаем над ними."},
+    {"#travel-menu-link": "Извините, поездки будут доступны позже. Мы уже работаем над ними."},
+    {"#shops-menu-link": "Извините, магазины будут доступны позже. Мы уже работаем над ними."},
+    {"#shops-menu-link": "Извините, магазины будут доступны позже. Мы уже работаем над ними."},
+    {"#shops-menu-link": "Извините, магазины будут доступны позже. Мы уже работаем над ними."},
+    {"#shops-menu-link": "Извините, магазины будут доступны позже. Мы уже работаем над ними."},
+  ],
+  ".pop-up-msg__warning-img",
+)
+
+//product sorter
+const boxForProduct = document.querySelector(".marketplace__products");
+
+function addProductInBox(photos, productLink, productName, price, date, ownerName, ownerLink, ownerRoom){
+  let productPhotosString = `<ul class="product__change-photo-box">`;
+  for (let photo of photos){
+    productPhotosString += `
+      <li class="product__change-photo-line"></li>
+      <img src="${photo}" alt="Изображение товара" class="product__img">
+    `;
+  }
+  productPhotosString += `</ul>`;
+  
+  const product = document.createElement("div");
+  product.classList.add("product");
+  product.classList.add("marketplace__product");
+  product.innerHTML = `
+    <a class="product__photo-container" href="${productLink}">
+      <img src="${photos[0]}" alt="Фотография ${productName}" class="product__mobile-photo">`
+      + productPhotosString +
+    `</a>
+    <div class="product__info-container">
+      <h3 class="h3"><a class="product__name" href="${productLink}">${productName}</a></h3>
+      <span class="product__price">${price}</span>
+      <div class="product__more-info">
+        <h4 class="h4"><a class="product__human" href="${ownerLink}">${ownerName}</a></h4>
+        <div class="product__more-than">
+          <span class="product__date">${date}</span>
+          <span class="product__room">${ownerRoom}</span>
+        </div>
+      </div>
+    </div>
+  `;
+  boxForProduct.append(product);
+}
 
 new SortBtns(
   ['sortByPopularity',
     () => {
-      console.log("Привет");
+      //top popularity
     },
     () => {
-      console.log("Пока");
+      //down popularity
     }],
   ['sortByDate',
     () => {
-      console.log("Привет22222");
+      //top date
     },
     () => {
-      console.log("Пока222222");
+      //down date
     }],
   ['sortByPrice',
     () => {
-      console.log("Привет333333");
+      //top price
     },
     () => {
-      console.log("Пока333333");
+      //down price
     }]
 );
+
+const photosForProduct = [
+  "source/product_img/f1.jpg",
+  "source/product_img/f2.jpg",
+  "source/product_img/f1.jpg",
+  "source/product_img/f2.jpg",
+]
+
+addProductInBox(photosForProduct, "link", "Название продукта 1", 100, "11 февраля", "Артём Бобцов", "link owner", "5501");
+
+
+//click also product btn
+document.querySelector(".marketplace__also-btn").addEventListener("click", (event) => {
+  event.target.style.display = "none";
+  document.querySelector(".marketplace__load-also-btn").style.display = "block";
+});
